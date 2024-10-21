0183d4136a96c8327076d4191060df0a6bdc4ed5
Victoria Farrell <victoria@yukaria.com>
2024-10-20 20:50:07 -0400

    Lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -17,6 +17,8 @@ func TestNextToken(t *testing.T) {
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
 		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
 		{token.COMMA, ","},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
